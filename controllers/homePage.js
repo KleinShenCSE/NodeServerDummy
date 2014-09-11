514527ca71cb071448b3ad3df5c26cd85ec83c0b
Ubuntu <ubuntu@ip-172-31-43-147.us-west-2.compute.internal>
2014-09-11 17:16:58 +0000

    merge

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,9 +1,13 @@
 var http = require('http');
+var fs = require('fs');
+var homePagePath = '../views/index.html';
+
 http.createServer(function (req, res) { 
-  console.log('!!');
-  console.log(req);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  fs.readFile(homePagePath, function () {
+    res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+    res.write(data);
+    res.end();
+  });
 }).listen(1337, '172.31.43.147');
 
 console.log('Server running at http://172.31.43.147:1337');
