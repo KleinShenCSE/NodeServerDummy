35b2813eaea48d4de41527ce1aaf9b5863851dd2
KleinShenCSE <kleinshen@gmail.com>
2014-09-11 13:14:03 -0400

    update the index page

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,9 +1,12 @@
+var hostdomain = 'http://172.31.43.147';
+var hostport = 1337;
+
 var http = require('http');
+// set up a sever
 http.createServer(function (req, res) { 
-  console.log('!!');
   console.log(req);
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, '172.31.43.147');
+}).listen(hostport, hostdomain);
 
-console.log('Server running at http://172.31.43.147:1337');
+console.log('Server running at ' + hostdomain + ':' + hostport);
