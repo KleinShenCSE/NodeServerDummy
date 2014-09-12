31bcbce28dcc42d94b026dbe0c91568623747138
Ubuntu <ubuntu@ip-172-31-43-147.us-west-2.compute.internal>
2014-09-12 03:31:23 +0000

    all end points working

--- controllers/router.js
+++ controllers/router.js
@@ -5,22 +5,29 @@ var indexPagePath = '../views/index.html';
 function route(request, response) {
   // get path to the request handler accodringly
   var pathname = url.parse(request.url, true).pathname;
-  switch (true) {
-    case (/\//.test(pathname)):
-      if (request.method === 'GET') {
-        displayIndexPage(request, response);
-      } else if (request.method === 'POST') {
-        displayInput(request, response);
-      }
-      break;
-    case (/\/set-cookie/.test(pathname)): 
-    
-      break;
-    case (/\/read-cookie/.test(pathname)):
 
+  switch (true) {
+    case (/\/set-cookie/.test(pathname) && request.method === 'GET'):
+      setCookie(request, response);
       break;
+
+    case (/\/read-cookie/.test(pathname) && request.method === 'GET'):
+      viewCookie(request, response);
+      break;
+
+    case (/\//.test(pathname) && request.method === 'GET'):
+      displayIndexPage(request, response);
+      break;
+
+    case (/\//.test(pathname) && request.method === 'POST'):
+      displayInput(request, response);
+      break;
+
     default:
       // route to 404 page, not match the url
+      response.writeHeader(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
   }
 }
 
@@ -41,6 +48,23 @@ function displayInput(req, res) {
     res.write(content);
     res.end();
   });
+}
+
+
+function setCookie(req, res) {
+  var d = new Date();
+  d.setMinutes(d.getMinutes() + 1);
+  res.writeHead(200, {
+    'Set-Cookie':'dummyCookieMessage; expires='+d.toUTCString()
+  });
+  res.end();
+}
+
+function viewCookie(req, res) {
+  var cookies = req.headers.cookie;
+  res.writeHead(200, {'Content-Type': 'text/html','Content-Length':cookies.length});
+  res.write(cookies);
+  res.end();
 }
 exports.route = route;
 
